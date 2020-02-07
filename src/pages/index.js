5a2681579bdecc6fb6bfab94fb86cf59a25ae1ec
Danilo Moreira de Castro <danilo.moreira94@gmail.com>
2020-02-07 13:36:48 -0300

    design: update hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import featureImage from "../../static/images/hero.png"
+import featureImage from "../../static/images/hero.svg"
 import thumbnailEvent from "../../static/images/feature-event.png"
 import thumbnailBoard from "../../static/images/feature-board.png"
 import thumbnailNews from "../../static/images/feature-news.png"
