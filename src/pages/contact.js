4846c5f6d8f31120b78867b02172f114d4b435ed
Danilo Moreira de Castro <danilo.moreira94@gmail.com>
2020-02-27 11:33:17 -0300

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,29 +4,35 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const ContactPage = () => (
-    <Layout>
-        <SEO title="Contact"/>
-        <div className={"container"}>
-            <div className={"content"}>
-                <div className={"title"}>
-                    <h1>Contact</h1>
-                </div>
-
-                <div className={"row"}>
-                    <div className={"col-7"}>
-                        <p>We take our commitment to our users seriously. If you need our help with your user account, have questions about how to use the HiStaff, any feedback or are experiencing technical difficulties, please do not hesitate to contact us.</p>
-                    </div>
-
-                    <div className={"col-5"}>
-                        <div className={"contact-items"}>
-                            <p>Plain email is the best way to contact us.</p>
-                            <h2>contact@histaff.io</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <Layout>
+    <SEO title="Contact" />
+    <div className={"container"}>
+      <div className={"content"}>
+        <div className={"title"}>
+          <h1>Contato</h1>
         </div>
-    </Layout>
+
+        <div className={"row"}>
+          <div className={"col-7"}>
+            <p>
+              Levamos a sério nosso compromisso com nossos usuários. Se você
+              precisar de nossa ajuda com sua conta de usuário, tiver dúvidas
+              sobre como usar os nossos serviços, tiver algum feedback ou
+              estiver enfrentando dificuldades técnicas, não hesite em entrar em
+              contato conosco.
+            </p>
+          </div>
+
+          <div className={"col-5"}>
+            <div className={"contact-items"}>
+              <p>E-mail é a ferramenta ideal para nos contactar:</p>
+              <h2>rodrigo@bromel.com.br</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
 )
 
 export default ContactPage
